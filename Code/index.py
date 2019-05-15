d301eea51f8bf26932b7553d1e1ac177619de95d
KhallilB <khallil.bailey@gmail.com>
2019-05-15 14:43:58 -0700

    source

--- Code/index.py
+++ Code/index.py
@@ -12,7 +12,7 @@ source_path = './texts/harry.txt'
 
 with open(source_path, 'r') as file:
     source = file.read()
-    
+
 start_stop_tokens=('\u0391', '\u03a9') 
 clean_source = cleanup.cleanup(source=source)
 tokens = get_tokens.generate(source=clean_source)
@@ -21,7 +21,7 @@ source_map = sentance_sampling.markov_path(token_list=tokens, order=order)
 
 @app.route('/')
 def index():
-    number_of_words = 10 if request.args.get('num') is None else int(request.args.get('num'))
+    number_of_words = 30 if request.args.get('num') is None else int(request.args.get('num'))
     sentence = sentance_sampling.markov_walk(path=source_map, distance=number_of_words, start_stop_tokens=start_stop_tokens)
     return sentence
 
